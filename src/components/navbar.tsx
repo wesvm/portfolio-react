ecd2d96b7111491ee34a3802cfbfac1738f2487d
blue <samuel_walter_@hotmail.com>
2023-12-07 18:39:10 -0500

    info & navbar

---
+++ src/components/navbar.tsx
@@ -0,0 +1,9 @@
+export const Navbar = () => {
+    return (
+        <>
+            <nav className="text-slate-100 bg-opacity-80 backdrop-blur-lg p-4">
+                nav
+            </nav>
+        </>
+    )
+}
