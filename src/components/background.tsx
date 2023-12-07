c1a62904a8735757fbcefcf9ff73b0da28e70900
blue <samuel_walter_@hotmail.com>
2023-12-07 18:38:13 -0500

    meteors background added

---
+++ src/components/background.tsx
@@ -0,0 +1,5 @@
+export const Background = () => (
+    <>
+        <div className='pointer-events-none absolute inset-0 h-screen bg-[radial-gradient(ellipse_80%_50%_at_50%_-20%,rgba(120,119,198,0.2),rgba(255,255,255,0))] dark:bg-[radial-gradient(ellipse_80%_50%_at_50%_-20%,rgba(120,119,198,0.2),rgba(255,255,255,0))]'></div>
+    </>
+)
